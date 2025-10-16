f269c44d23aff598d59eee9afb577e61e37b12d8
Bang Gong <gongbang@cafu.cs.unc.edu>
2025-10-16 03:43:17 -0400

    release code

--- src/run_helper_attention.sh
+++
@@ -1,4 +0,0 @@
-python helper_attention.py \
-    --base_path /playpen-nas-ssd/gongbang/try_code_features \
-    --output_dir_averages /playpen-nas-ssd/gongbang/try_code_age_groups_kv \
-    --output_dir_direction /playpen-nas-ssd/gongbang/try_code_aging_directions
